959d70a907b247ddcc36e869c03329043953d8b0
phymilad <phy.milad@gmail.com>
2025-04-27 10:24:51 +0330

    fix: add relation for BlogLikeEntity

--- src/modules/blog/entities/like.entity.ts
+++ src/modules/blog/entities/like.entity.ts
@@ -2,11 +2,15 @@ import { BaseEntity } from "src/common/abstracts/base.entity";
 import { EntityNames } from "src/common/enums/entity.enum";
 import { UserEntity } from "src/modules/user/entities/user.entity";
 import { Column, Entity, ManyToOne } from "typeorm";
+import { BlogEntity } from "./blog.entity";
 
 @Entity(EntityNames.BlogLike)
 export class BlogLikeEntity extends BaseEntity {
+    // Relation with BlogEntity
     @Column()
     blogId: number;
+    @ManyToOne(() => BlogEntity, (blog) => blog.likes, {onDelete: 'CASCADE'})
+    blog: BlogEntity
 
     // Relation with UserEntity
     @Column()
